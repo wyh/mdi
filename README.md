537e25f72eedf1d12361a8dc7ee436a4b6f3235e
Samuel Wu <samuel.yh.wu@gmail.com>
2020-04-02 14:04:34 +0800

    Added material design icon component

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# mdi
-A uni-app icon component
+# 介绍
+
+Material Design Icon(MDI) 提供了丰富的设计一致的免费icon 可以使用.
+相比国内的icon设计，MDI 的设计符合Android Material Design, 保持一致。
+
+阿里云也提供了很多丰富的icon, 但很多icon 不一致，使用的时候需要非常小心。
+
+# 使用
+
+1. 下载component 后放入到compoments/ 文件夹下
+2. import components
+3. 按照下面的方法使用： 
+
+```
+<mdi color="red" size="30" icon="mdi-hand-water"></mdi>
+```
+
+# Thanks
+
+All of the icons could be found: [Material Design Icons](materialdesignicons.com).
+And a cheat sheet: https://cdn.materialdesignicons.com/5.0.45/
---
+++ mdi.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+		<text class="mdi" :class="icon" :style="style"></text>     
+	</view>
+</template>
+
+<script>
+    import "./materialdesignicons.min.css";
+	export default {
+        props: {
+            icon: {
+                type: String
+            }, 
+            color: {
+                type: String
+            },
+            size: {
+                type: Number,
+                default: 20
+            }
+        },
+		data() {
+			return {
+				
+			};
+		},
+        computed:{
+            style() {
+                
+                return `color: ${this.color}; font-size: ${this.size}px`;
+            }
+        }
+	}
+</script>
+
+<style lang="less">
+
+</style>
